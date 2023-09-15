09fe65c8085ba8b0fbe64bbc82e0428db7de7f24
iamshakur <salmanshakur7@gmail.com>
2023-09-15 10:36:31 +0100

    recoverpsswd and recoverlink page added

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../styles/dashboard.css";
 import SideBar from "../components/sidebar";
 import Header from "../components/header";
+import WalletSection from "../components/dashboardComponent/walletSection";
 
 function Dashboard() {
 	return (
@@ -18,40 +19,7 @@ function Dashboard() {
 function MainContent() {
 	return (
 		<section className="main-content">
-			<section className="wallet-section">
-				<p>
-					Welcome <span className="welcomename">,</span>{" "}
-					<span className="account">Account Number: 2352672636 Wema Bank</span>
-				</p>
-				<div className="balance-container">
-					<div className="balance">
-						<i className="fa-solid fa-wallet fa-2x"></i>
-						<p>Wallet Balance</p>
-						<h4 className="amount">0.00</h4>
-					</div>
-					<div className="balance">
-						<i className="fa-solid fa-wallet fa-2x"></i>
-						<p>Airtel CG Portal Balance</p>
-						<h4 className="airtelCG">0.00</h4>
-					</div>
-					<div className="balance">
-						<i className="fa-solid fa-wallet fa-2x"></i>
-						<p>Mtn SME Portal Balance</p>
-						<h4 className="mtnSME">0.00</h4>
-					</div>
-					<div className="balance">
-						<i className="fa-solid fa-wallet fa-2x"></i>
-						<p>Mtn Data Gifting Balance</p>
-						<h4 className="mtnCG">0.00</h4>
-					</div>
-					<div className="balance">
-						<i className="fa-solid fa-wallet fa-2x"></i>
-						<p>More</p>
-					</div>
-				</div>
-			</section>
-
-			{/* Quick Links Section */}
+			<WalletSection />
 			<section className="quick-links">
 				<h2>Quick Links</h2>
 				<div className="links-container">
--- src/routes/pagesRouter.js
+++ src/routes/pagesRouter.js
@@ -2,10 +2,14 @@ import React from "react";
 import Register from "../pages/signup";
 import Login from "../pages/login";
 import Dashboard from "../pages/dashboard";
+import RecoverPassword from "../pages/recoverPassword";
+import RecoverLink from "../pages/recoverLink";
 
 const pageRoutes = [
 	{ path: "/signup", element: <Register /> },
 	{ path: "/login", element: <Login /> },
 	{ path: "/dashboard", element: <Dashboard /> },
+	{ path: "/recoverpassword", element: <RecoverPassword /> },
+	{ path: "/recoverlink", element: <RecoverLink /> },
 ];
 export default pageRoutes;
